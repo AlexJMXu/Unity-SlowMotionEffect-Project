bd35fba7d5ad7e57f11f699180b2bac87fec91a7
Alexander Xu <ajm.xu@outlook.com>
2017-03-02 01:47:46 +0000

    Initial Commit

---
+++ Assets/Scripts/ExplosionGun.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionGun : MonoBehaviour {
+
+	public GameObject explosion;
+	public Camera cam;
+
+	public TimeManager timeManager;
+	
+	void Update () {
+		if (Input.GetButtonDown("Fire1")) Shoot();
+	}
+
+	void Shoot() {
+		RaycastHit _hitInfo;
+
+		if (Physics.Raycast(cam.transform.position, cam.transform.forward, out _hitInfo)) {
+			GameObject _effect = (GameObject) Instantiate(explosion, _hitInfo.point, Quaternion.LookRotation(_hitInfo.normal));
+
+			timeManager.SlowMotion();
+
+			Destroy(_effect, 5f);
+		}
+	}
+}
